5723ac917642969cc5fed4b5bd5069faafe2e5d8
nithish847 <nithish.gujjala07@gmail.com>
2025-07-30 23:38:07 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 const corsOptions={
     origin:'http://localhost:5173',
+    "https://online-order-b.onrender.com",
     credentials:true
 }
 app.use(cors(corsOptions))
